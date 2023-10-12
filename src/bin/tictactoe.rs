e5b674bd7d9317ded0602c135ac7436526a4d0d7
Harry Linrui XU <xulr0820@hotmail.com>
2023-10-12 20:24:37 +0200

    First commit for tictactoe project

---
+++ src/bin/tictactoe.rs
@@ -0,0 +1,17 @@
+fn main() {
+    
+}
+
+//Struct for square  - node in grid
+//Contain enum for state (empty, x, o)
+struct Node {
+    state:State,
+}
+
+enum State {
+    Empty,
+    X,
+    O,
+}
+
+struct Grid {
---
+++ src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+//Struct for square  - node in grid
+//Contain enum for state (empty, x, o)
+struct Node {
+    state:State,
+}
+
+enum State {
+    Empty,
+    X,
+    O,
+}
+
+struct Grid {
+
+}
+
+
+
