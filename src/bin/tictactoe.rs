a47bad8bcd895b1207d30f1877d919202350704c
Harry Linrui XU <xulr0820@hotmail.com>
2023-10-18 17:38:41 +0200

    Removed usage of Array2

--- src/bin/tictactoe.rs
+++ src/bin/tictactoe.rs
@@ -1,20 +1,24 @@
-use ndarray::Array2;
-
 #[derive(Debug)] //Enable printing out complex data structures
 struct TicTacToeGrid {
-    cells: Array2<State>
+    cells: [[State; 3]; 3]
 }
 
 impl TicTacToeGrid {
     fn new() -> Self {
         // Create a new 3x3 grid with all squares set to Empty
         Self {
-            cells: Array2::from_elem((3, 3), State::Empty),
+            cells: [[State::Empty; 3]; 3],
         }
     }
 }
 
-#[derive(Debug, Clone)] //Enable printing out complex data structures
+impl TicTacToeGrid {
+    fn pick_space() -> bool{
+        true
+    }
+}
+
+#[derive(Debug, Clone, Copy)] //Enable printing out complex data structures
 enum State {
     Empty,
     X,
@@ -28,10 +32,17 @@ enum GameProgress {
 
 fn main() {
     let mut grid: TicTacToeGrid = TicTacToeGrid::new();
+    grid.cells[1][1] = State::O
+
+
     //player 1 starter
         //Velger en node ved å enten skrive inn kordinater eller rutenr (1-9)
         //Sjekker om noden er ledig
         //Sjekker om win
     //player 2 fortsetter
         //Repeat until win
+}
+
+fn draw_grid(grid: TicTacToeGrid) {
+
 }
