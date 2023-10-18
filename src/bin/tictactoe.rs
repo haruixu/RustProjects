f4861c30adf8f3ad8bbaf15191efc0c8c74bdb01
Harry Linrui XU <xulr0820@hotmail.com>
2023-10-18 15:22:32 +0200

    Created base structs and enums for tictactoe game

--- src/bin/tictactoe.rs
+++ src/bin/tictactoe.rs
@@ -1,17 +1,37 @@
-fn main() {
-    
+use ndarray::Array2;
+
+#[derive(Debug)] //Enable printing out complex data structures
+struct TicTacToeGrid {
+    cells: Array2<State>
 }
 
-//Struct for square  - node in grid
-//Contain enum for state (empty, x, o)
-struct Node {
-    state:State,
+impl TicTacToeGrid {
+    fn new() -> Self {
+        // Create a new 3x3 grid with all squares set to Empty
+        Self {
+            cells: Array2::from_elem((3, 3), State::Empty),
+        }
+    }
 }
 
+#[derive(Debug, Clone)] //Enable printing out complex data structures
 enum State {
     Empty,
     X,
     O,
 }
 
-struct Grid {
+enum GameProgress {
+    Finished,
+    OnGoing,
+}
+
+fn main() {
+    let mut grid: TicTacToeGrid = TicTacToeGrid::new();
+    //player 1 starter
+        //Velger en node ved å enten skrive inn kordinater eller rutenr (1-9)
+        //Sjekker om noden er ledig
+        //Sjekker om win
+    //player 2 fortsetter
+        //Repeat until win
+}
